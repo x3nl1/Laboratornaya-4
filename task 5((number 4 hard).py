a74f98f40da1d85b29b139b7d918d46eea518bb3
x3nl1 <stepanagapov2005@gmail.com>
2025-11-27 00:59:31 +0300

    Create task 5((number 4 hard).py
    completed task 5

---
+++ task 5((number 4 hard).py
@@ -0,0 +1,14 @@
+def filter_file_data(filename, condition):
+    with open(filename, 'r') as file:
+        for line in file:
+            if condition(line.strip()):
+                yield line.strip()
+
+def is_long_line(line):
+    return len(line) > 10
+
+def contains_python(line):
+    return 'python' in line.lower()
+
+filtered_long_lines = list(filter_file_data('data.txt', is_long_line))
+filtered_python_lines = list(filter_file_data('data.txt', contains_python))

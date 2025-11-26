fa12cb0f2b1f1a788c0c6abd81cca74d3e593fff
x3nl1 <stepanagapov2005@gmail.com>
2025-11-27 00:55:04 +0300

    Update task 2(number 3 easy).py
    updated task 2

--- task 2(number 3 easy).py
+++ task 2(number 3 easy).py
@@ -3,6 +3,6 @@ from functools import reduce
 def sum_numbers(numbers):
     return reduce(lambda acc, x: acc + x, numbers)
 
-numbers = [1, 2}
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 total_sum = sum_numbers(numbers)
 print(total_sum)
